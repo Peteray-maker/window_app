26ebab303e6428f672208588b5c92a87859e0f54
Popoola, Ayomide <67739488+Peteray-maker@users.noreply.github.com>
2024-03-19 22:35:48 +0000

    Add files via upload

---
+++ testing/IntQueue.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testing
+{
+    class IntQueue
+    {
+        private int maxsize = 30;
+        private int[] store;
+        private int head = 0;
+        private int tail = 0;
+        private int numItems = 0;
+
+        // Default constructor
+        public IntQueue()
+        {
+            store = new int[maxsize];
+        }
+
+        // Constructor with a specified maximum size
+        public IntQueue(int size)
+        {
+            maxsize = size;
+            store = new int[maxsize];
+        }
+
+        public void Enqueue(int value)
+        {
+            store[tail] = value;
+            tail = tail + 1;
+            numItems++;
+        }
+
+        public int Dequeue()
+        {
+            int value = store[head];
+            head = head + 1;
+            numItems--;
+            return value;
+        }
+
+        public int Peek()
+        {
+            return store[head];
+        }
+
+        public bool IsEmpty()
+        {
+            return numItems == 0;
+        }
+
+        public bool IsFull()
+        {
+            return numItems == maxsize;
+        }
+
+        public string display()
+        {
+            StringBuilder output = new StringBuilder();
+            for(int i=0; i<=tail-1; i++)
+            {
+                output.Append(store[i] + ","); 
+            }
+            return output.ToString();
+        }
+
+
+    }
+}
---
+++ testing/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntQueue mine = new IntQueue();
+
+            mine.Enqueue(30);
+            mine.Enqueue(40);
+            mine.Enqueue(50);
+            mine.Enqueue(60);
+            mine.Enqueue(70);
+            mine.Enqueue(80);
+
+            Console.WriteLine(mine.display());
+
+            mine.Dequeue();
+            mine.Dequeue();
+
+            Console.WriteLine(mine.Dequeue());
+            Console.WriteLine(mine.Dequeue());
+            Console.WriteLine(mine.Dequeue());
+
+            Console.WriteLine(mine.Peek());
+
+            Console.ReadKey();
+        }
+
+        
+
+    }
+}
